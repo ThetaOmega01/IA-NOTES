28b5f483bb7fe689f434d76d1dd43309e15d674d
ThetaOmega01 <zjhztjf@gmail.com>
2025-06-04 14:11:42 +0800

    - Breaking change: Refactor header

--- Michaelmas/build.bat
+++
@@ -1,2 +0,0 @@
-latexmk -synctex=1 -interaction=nonstopmode -file-line-error -pdf *.tex
-del *.aux *.fdb_latexmk *.fls *.out *.toc *.log /f /q
--- Michaelmas/build.sh
+++
@@ -1,2 +0,0 @@
-latexmk -synctex=1 -interaction=nonstopmode -file-line-error -pdf *.tex
-rm *.aux *.fdb_latexmk *.fls *.out *.toc *.log
